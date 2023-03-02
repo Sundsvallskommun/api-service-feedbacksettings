9d5a997c29b30a5f70ec4457439a6a911a9bc3aa
Joakim Schürer <joakim.schurer@sundsvall.se>
2023-03-02 11:29:22 +0100

    UF-4484: Adjusted circuit breaker settings
    - Added transactional annotation to repository interface
    - Updated Dept44 to version 1.30

--- src/main/java/se/sundsvall/feedbacksettings/integration/db/FeedbackSettingsRepository.java
+++ src/main/java/se/sundsvall/feedbacksettings/integration/db/FeedbackSettingsRepository.java
@@ -1,10 +1,13 @@
 package se.sundsvall.feedbacksettings.integration.db;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.QueryByExampleExecutor;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import se.sundsvall.feedbacksettings.integration.db.model.FeedbackSettingEntity;
 
+@Transactional
 @CircuitBreaker(name = "feedbackSettingsRepository")
 public interface FeedbackSettingsRepository extends PagingAndSortingRepository<FeedbackSettingEntity, String>, QueryByExampleExecutor<FeedbackSettingEntity> {}
